e35a068831dd40f83cfe4ac00c8d9ea10dc2b972
Chris Henderson <chris@app-press.com>
2017-01-28 23:47:44 -0500

    Add multi cfid processing

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const config = require("./lib/config");
 
 const async = require("async");
 const AWS = require("aws-sdk");
-const bunyan = require("bunyan");
+const debug = require("debug")("cf");
 const moment = require("moment");
-const processHour = require("./lib/processHour");
 const processDay = require("./lib/processDay");
 
 const lastProcessableDate = moment().subtract(45, "days").startOf("day");
@@ -18,45 +17,121 @@ const s3 = new AWS.S3({
 });
 let listObjectResults;
 
-async.during(
-	(cb) => {
-		s3.listObjectsV2({ MaxKeys: 2, Prefix: config.prefix }, (err, data) => {
-			if (err) {
-				return cb(err);
-			}
+if (config.cfids && config.cfids.length) {
+	debug("CFID(s) specified, processing each in series");
+	async.eachLimit(config.cfids, 1, processDistribution, reportAndExit);
+} else {
+	debug("No CFIDs specified, processing all records");
+	processDistribution(null, reportAndExit);
+}
 
-			if (!data.Contents.length) {
-				return cb(null, false);
-			}
+function processDistribution(cfid, cb) {
+	const prefix = `${config.prefix || ""}${cfid || ""}`;
 
-			listObjectResults = data;
+	debug(`Searching for records in ${config.bucket}/${prefix}`);
 
-			if (data.Contents[0].Key.slice(-1) === "/") {
-				if (data.Contents[1]) {
-					return cb(null, shouldProcess(data.Contents[1].Key));
-				}
-				return cb();
-			}
+	async.during(
+		(cb) => probe(prefix, null, cb),
+		(cb) => processDay(s3, listObjectResults, cb),
+		cb
+	);
+}
 
-			cb(null, shouldProcess(data.Contents[0].Key));
-		});
-	},
-	(cb) => processDay(s3, listObjectResults, cb),
-	(err) => {
+function probe(prefix, token, cb) {
+	const listObjectsConfig = { MaxKeys: 2, Prefix: prefix, ContinuationToken: token };
+
+	s3.listObjectsV2(listObjectsConfig, (err, data) => {
 		if (err) {
-			console.error(err);
-			process.exit(1);
+			return cb(err);
 		}
 
-		console.log("DONE");
-		process.exit(0);
+		if (!data.Contents.length && !data.IsTruncated) {
+			debug("No results returned and response is not truncated, exiting");
+			return cb(null, false);
+		}
+
+		if (!data.Contents.length && data.IsTruncated) {
+			debug("No results returned but response is truncated, probing again");
+			return probe(prefix, data.NextContinuationToken, cb);
+		}
+
+		listObjectResults = data;
+
+		if (data.Contents[0].Key.slice(-1) === "/") {
+			debug("First result is a folder");
+
+			if (data.Contents[1]) {
+				return analyzeResult(1);
+			}
+		}
+
+		debug("First result is a file");
+		analyzeResult(0);
+
+		function analyzeResult(index) {
+			const isProcessable = shouldProcess(data.Contents[index].Key);
+
+			if (isProcessable) {
+				debug(`Result [${index}] is processable`);
+				return cb(null, true);
+			}
+
+			if (data.isTruncated) {
+				debug(`Result [${index}] is not processable, but response is truncated, probing again`);
+				return probe(prefix, data.NextContinuationToken, cb);
+			}
+
+			debug("Results are not processable");
+			cb();
+		}
+	});
+}
+
+function reportAndExit(err) {
+	if (err) {
+		console.error(err);
+		process.exit(1);
 	}
-);
+
+	console.log("DONE");
+	process.exit(0);
+}
 
 function shouldProcess(key) {
-	const filename = key.substring(key.lastIndexOf(config.prefix) + config.prefix.length);
-	const dateTime = filename.split(".")[1];
-	const date = moment(dateTime.substring(0, 10));
+	const filename = key.split("/").pop();
+	const dateTime = filename.split(".")[1] || "";
+	const dateTimeParts = dateTime.split("-").map((part) => {
+		if (isNaN(parseInt(part, 10))) {
+			return false;
+		}
+		return parseInt(part, 10);
+	});
 
-	return date.isBefore(lastProcessableDate);
+	debug(`Inspecting ${filename}`);
+
+	if (filename.split(".").length !== 4 || dateTimeParts.length !== 4) {
+		debug("Filename doesn't match pattern; not processable");
+		return false;
+	}
+
+	if (!dateTimeParts.every((part) => typeof part === "number")) {
+		debug("Date parts are not all numbers; not processable");
+		return false;
+	}
+
+	const date = moment([dateTimeParts[0], dateTimeParts[1] - 1, dateTimeParts[2], dateTimeParts[3]]);
+	const processable = date.isBefore(lastProcessableDate);
+
+	if (!date.isValid()) {
+		debug("Invalid date when constructed; not processable");
+		return false;
+	}
+
+	if (processable) {
+		debug("File falls within acceptable date range; proceeding");
+	} else {
+		debug("File is after processable date range");
+	}
+
+	return processable;
 }
